f2e136f17add432ae851c4f6f1f6e1fd7d04b70d
Yagnesh Modh <contact@thespecialcharacter.com>
2023-12-20 11:24:23 +0530

    added cors

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+app.use(cors())
+
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function (req, res) {
