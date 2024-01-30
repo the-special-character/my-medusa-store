c9c645d0c1f288ef7419106ba53f613e6054cda7
Yagnesh Modh <contact@thespecialcharacter.com>
2024-01-30 14:24:37 +0530

    commit code

--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@
 # Specify the folder path
 folder_path="./build/public/locales"
 
+existing_logo_path="./build/public/logo.svg"
+
+new_logo_path="./logo.svg"
+
 # Iterate through each JSON file in the folder recursively using find
 find "$folder_path" -type f -name "*.json" | while read -r file; do
     # Check if the file exists and is readable
@@ -21,5 +25,14 @@ find "$folder_path" -type f -name "*.json" | while read -r file; do
         echo "Error: Unable to read file - $file"
     fi
 done
+
+if [ -r "$new_logo_path" ]; then
+    # Replace the existing logo with the new logo
+    cp "$new_logo_path" "$existing_logo_path"
+    echo "Logo replaced successfully."
+else
+    echo "Error: New logo file not found or not readable - $new_logo_path"
+fi
+
 
 echo "Replacement process completed."
